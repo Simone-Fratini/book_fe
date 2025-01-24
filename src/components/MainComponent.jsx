cde7371a29017b34c259bc8ce6734030b4de56fe
Aj-Herrera-99 <ajherrera999@gmail.com>
2025-01-24 18:52:08 +0100

    MainComp conversione bootstrap -> tailwind

--- src/components/MainComponent.jsx
+++ src/components/MainComponent.jsx
@@ -4,32 +4,34 @@ import Card from "./Card";
 import axios from "axios";
 
 export default function MainComponent() {
-  const [books, setBooks] = useState([]);
+    const [books, setBooks] = useState([]);
 
-  const apiUrl = import.meta.env.VITE_BOOKS_URL;
+    const apiUrl = import.meta.env.VITE_BOOKS_URL;
 
-  const fetchBooks = () => {
-    axios.get(`${apiUrl}`).then((res) => {
-      setBooks(res.data);
-      console.log(res.data);
-    });
-  };
+    const fetchBooks = () => {
+        axios.get(`${apiUrl}`).then((res) => {
+            setBooks(res.data);
+            console.log(res.data);
+        });
+    };
 
-  useEffect(() => {
-    fetchBooks();
-  }, []);
+    useEffect(() => {
+        fetchBooks();
+    }, []);
 
-  return (
-    <div className="container my-4">
-      <h1 className="text-primary">Bool Books</h1>
-      <h2 className="py-2 fst-italic">The nerdest book comunity</h2>
-      <div className="row row-cols-1 row-cols-md-3 g-3">
-        {books.map((book) => (
-          <div className="col" key={book.id}>
-            <Card book={book} />
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+    return (
+        <>
+            <div className="mx-16 my-4">
+                <h1 className="text-blue-600">Bool Books</h1>
+                <h2 className="py-2 italic">The nerdest book comunity</h2>
+                <div className="grid grid-cols-1 gap-3 md:grid-cols-3">
+                    {books.map((book) => (
+                        <div key={book.id}>
+                            <Card book={book} />
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </>
+    );
 }
