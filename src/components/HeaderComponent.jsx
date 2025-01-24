d64071aa5cbe1c3b1a50d9d30048efa695dda5a7
Simone-Fratini <simone@fratini.com>
2025-01-24 11:55:21 +0100

    Update HeaderComponent styling with rounded corners and increased padding

--- src/components/HeaderComponent.jsx
+++ src/components/HeaderComponent.jsx
@@ -3,14 +3,14 @@ import { Link } from "react-router-dom";
 
 function HeaderComponent() {
   return (
-    <header class="bg-blue-800 text-white p-4 shadow-md">
-      <div class="container mx-auto flex items-center justify-between">
-        <div class="text-2xl font-bold">Boolean Book-Shop</div>
-        <nav class="flex space-x-4">
-            <Link to="/" className="hover:text-blue-300">Home</Link>
-            <Link to="/about" className="hover:text-blue-300">About</Link>
-        </nav>
-      </div>        
+    <header class="bg-blue-800 text-white p-6 shadow-md rounded-b-lg">
+        <div class="container mx-auto flex items-center justify-between">
+            <div class="text-2xl font-bold">Boolean Book-Shop</div>
+            <nav class="flex space-x-4">
+                <Link to="/" className="hover:text-blue-300">Home</Link>
+                <Link to="/about" className="hover:text-blue-300">About</Link>
+            </nav>
+        </div>
     </header>
   )
 }
