bb280419c137115245f7cfeffb4d79c9f88ed411
Simone-Fratini <simone@fratini.com>
2025-01-24 11:45:27 +0100

    Enhance HeaderComponent with navigation links and styling

--- src/components/HeaderComponent.jsx
+++ src/components/HeaderComponent.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import About from "./pages/About";
 
 function HeaderComponent() {
   return (
-    <div>HeaderComponent</div>
+    <header class="bg-blue-800 text-white p-4 shadow-md">
+      <div class="container mx-auto flex items-center justify-between">
+        <div class="text-2xl font-bold">Boolean Book-Shop</div>
+        <nav class="flex space-x-4">
+            <Link to="/" className="hover:text-blue-300">Home</Link>
+            <Link to="/about" className="hover:text-blue-300">About</Link>
+        </nav>
+      </div>        
+</header>
   )
 }
 
