276ab4ab65f38e3fb47b012416d59e18f9f5d5f5
Aj-Herrera-99 <ajherrera999@gmail.com>
2025-01-24 11:49:55 +0100

    routing fatto

--- src/pages/DefaultLayout.jsx
+++ src/pages/DefaultLayout.jsx
@@ -1,16 +1,18 @@
-import React from 'react'
-import HeaderComponent from '../components/Header'
-import FooterComponent from '../components/Footer'
+import React from "react";
+import HeaderComponent from "../components/HeaderComponent";
+import FooterComponent from "../components/FooterComponent";
+import { Outlet } from "react-router-dom";
 
 function DefaultLayout() {
-  return (
-    <>
-    <HeaderComponent />
-    
-    <FooterComponent />
-    </>
-
-  )
+    return (
+        <>
+            <HeaderComponent />
+            <main>
+                <Outlet />
+            </main>
+            <FooterComponent />
+        </>
+    );
 }
 
-export default DefaultLayout
+export default DefaultLayout;
