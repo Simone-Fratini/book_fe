9464dd4b009602dad72be1cb4f52372d972ee1f0
Simone Fratini <128401030+Simone-Fratini@users.noreply.github.com>
2025-01-24 11:16:09 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# My React Template
+
+# Libraries installed
+
+-   Tailwind css
+-   react-router-dom
+-   axios
---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+
+function App() {
+    return (
+        <>
+            <h1 className="w-full py-4 text-6xl text-center">
+                My React Template
+            </h1>
+            <h2 className="py-4 text-3xl text-center">Packages installed:</h2>
+            <ul className="text-center">
+                <li>Tailwind css</li>
+                <li>react-router-dom</li>
+                <li>axios</li>
+            </ul>
+        </>
+    );
+}
+
+export default App;
