101a4fb716ccb183722e02f8609a237314b1595c
Simone-Fratini <simone@fratini.com>
2025-01-25 17:11:52 +0100

    Add dark mode toggle to HeaderComponent and update Tailwind config

--- src/components/HeaderComponent.jsx
+++ src/components/HeaderComponent.jsx
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 import { Link } from "react-router-dom";
 import { FaHome } from "react-icons/fa";
+import { FiSun } from "react-icons/fi";
+import { FaMoon } from "react-icons/fa6";
 import { IoInformationCircleOutline } from "react-icons/io5";
 
 function HeaderComponent() {
+
+    const [darkMode, setDarkMode] = useState(false);
+
+    useEffect(() => {
+    if (darkMode) {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+  }, [darkMode]);
+
   return (
-    <header className="bg-gradient-to-r from-blue-800 to-blue-600 text-white p-6 shadow-lg rounded-b-2xl">
+    <header className="bg-gradient-to-r dark:from-gray-800 dark:to-gray-600 from-blue-800 to-blue-600 text-white p-6 shadow-lg rounded-b-2xl">
         <div className="container mx-auto flex items-center justify-between">
             {/* Logo */}
             <div className="font-extrabold text-xl sm:text-xl md:text-2xl lg:text-3xl">Boolean Book-Shop</div>
@@ -20,6 +33,11 @@ function HeaderComponent() {
                     <IoInformationCircleOutline className="text-xl" />
                     <span>About</span>
                 </Link>
+
+                {/* Dark Mode Button */}
+                <button onClick={() => setDarkMode(!darkMode)} className="p-2 border rounded-full  hover:bg-gray-200 hover:text-gray-500 transition duration-300">
+                    {darkMode ? <FiSun className="text-md" /> : <FaMoon className="text-md" />}
+                </button>
             </nav>
         </div>
     </header>
