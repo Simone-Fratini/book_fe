9b5b62c6aac882f1451262375de440707efc5422
Aj-Herrera-99 <ajherrera999@gmail.com>
2025-01-25 19:01:16 +0100

    aggiunta immagini

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,16 +1,17 @@
 export default function Card({ book }) {
   return (
-    <div className="p-4 bg-blue-300" style={{ width: "100%" }}>
-      <img
-        src={book.image}
-        className="card-img-top"
-        alt={book.title}
-        style={{ objectFit: "cover", height: "350px" }}
-      />
+    <div className="p-4" style={{ width: "100%" }}>
       <div>
-        <h5 className="text-3xl">{book.title}</h5>
-        <p className="text-xl italic">{book.author}</p>
-        <p className="card-text">{book.abstract}</p>
+        <img
+          src={`${import.meta.env.VITE_BOOK_IMG_URL}/${book.image}`}
+          className="object-contain h-[350px] mx-auto"
+          alt={book.title}
+        />
+      </div>
+      <div>
+        <h3 className="mt-1 text-3xl">{book.title}</h3>
+        <h4 className="text-xl italic">{book.author}</h4>
+        <p className="my-2">{book.abstract}</p>
         <button className="px-4 py-1 text-white bg-blue-600 rounded-xl">Dettagli</button>
       </div>
     </div>
--- src/components/MainComponent.jsx
+++ src/components/MainComponent.jsx
@@ -15,15 +15,15 @@ export default function MainComponent() {
         });
     };
 
-    // useEffect(() => {
-    //     fetchBooks();
-    // }, []);
+    useEffect(() => {
+        fetchBooks();
+    }, []);
 
   return (
     <div className="mx-16 my-4">
-      <h1 className="text-blue-600">Bool Books</h1>
-      <h2 className="py-2 italic">The nerdest book comunity</h2>
-      <div className="grid grid-cols-1 gap-3 md:grid-cols-3">
+      <h1 className="text-5xl font-bold tracking-wide text-blue-600">Bool Books</h1>
+      <h2 className="py-2 text-3xl italic font-semibold">The nerdest book comunity</h2>
+      <div className="grid grid-cols-1 gap-10 md:grid-cols-3 md:mx-24">
         {books.map((book) => (
           <div key={book.id}>
             <Card book={book} />
