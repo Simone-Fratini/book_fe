0f10778d3b409fa232b9932e0588f99a4ab81359
Simone-Fratini <simone@fratini.com>
2025-01-26 14:56:44 +0100

    Add ErrorPage component and update routing in App.jsx; include motion dependency

---
+++ src/pages/ErrorPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { motion } from "framer-motion";
+
+function ErrorPage() {
+  return (
+    <>
+        <div className='h-screen flex justify-center items-center bg-gradient-to-br from-blue-400 via-blue-800 to-blue-950'>
+            ciao
+        </div>
+    </>
+  )
+}
+
+export default ErrorPage
