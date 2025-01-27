8a5e53003ec2d2e05b81045aa180bda090129b56
Simone-Fratini <simone@fratini.com>
2025-01-27 20:22:58 +0100

    Refactor HeroPage to use motion components for animated link and icon interactions

--- src/pages/HeroPage.jsx
+++ src/pages/HeroPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
-import { animationControls, motion } from 'framer-motion';
+import { motion } from 'framer-motion';
+import { FaBook } from "react-icons/fa";
 
 function HeroPage() {
 
@@ -19,6 +20,18 @@ function HeroPage() {
     }
   }
 
+  const iconVariants = {
+    initial: { x: 0 }, 
+    hover: { x: 38 },
+  };
+
+  const textVariants = {
+    initial: { opacity: 1, x: 0 }, 
+    hover: { opacity: 0, x: 100 }, 
+  };
+
+  const MotionLink = motion(Link)
+
   return (
     <div className="h-screen bg-cover bg-center relative" style={{backgroundImage: "url('/img/hero_img.jpg')" }}>
       {/* Overlay */}
@@ -32,7 +45,17 @@ function HeroPage() {
         animate="visible">
           <h1 className="text-4xl md:text-5xl font-bold mb-4">Boolean Book Library</h1>
           <p className="text-lg mb-6">Discover thousands of books to inspire, educate, and entertain.</p>
-          <Link to="/home" className="px-6 py-3 text-lg font-medium bg-blue-600 hover:bg-blue-700 text-white rounded-lg shadow-lg transition duration-300">Browse the Catalog</Link>
+          <MotionLink
+            initial="initial"
+            whileHover="hover"
+            animate="rest" to="/home" className=" cursor-pointer flex items-center justify-center gap-2 w-1/2 md:w-1/3 lg:w-1/3  py-2 text-sm md:text-lg font-medium bg-blue-600 hover:bg-blue-700 text-white rounded-lg shadow-lg transition duration-300 mx-auto overflow-hidden">
+          <motion.span 
+            variants={iconVariants}
+            transition={{ duration: 0.4 }}><FaBook /></motion.span>
+          <motion.span 
+            variants={textVariants}
+            transition={{ duration: 0.4 }}>Catalog</motion.span>
+          </MotionLink>
         </motion.div>
       </div>
     </div>
