e325007018d28682a597da586a954ac954ba63b9
Simone-Fratini <simone@fratini.com>
2025-01-27 10:35:08 +0100

    Style updates: add background color to body, replace button with link in Card component, and enhance layout in MainComponent

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 export default function Card({ book }) {
   return (
     <div className="p-4" style={{ width: "100%" }}>
@@ -8,11 +10,13 @@ export default function Card({ book }) {
           alt={book.title}
         />
       </div>
-      <div>
+      <div className="">
         <h3 className="mt-1 text-3xl">{book.title}</h3>
         <h4 className="text-xl italic">{book.author}</h4>
         <p className="my-2">{book.abstract}</p>
-        <button className="px-4 py-1 text-white bg-blue-600 rounded-xl">Dettagli</button>
+        <Link to={`/home/${book.id}`}>
+          <button className="px-4 py-1 text-white bg-blue-600 rounded-xl">Dettagli</button>
+        </Link>
       </div>
     </div>
   );
